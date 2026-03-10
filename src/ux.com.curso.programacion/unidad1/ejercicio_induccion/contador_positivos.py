0f2189b5809f479efe6ce8b012890b71197987e3
Emiliano <rubenemilianomelchorvilla@gmail.com>
2026-03-10 07:40:09 -0600

    comentarios

--- src/ux.com.curso.programacion/unidad1/ejercicio_induccion/contador_positivos.py
+++ src/ux.com.curso.programacion/unidad1/ejercicio_induccion/contador_positivos.py
@@ -1,6 +1,10 @@
 #desarrollo de algoritmos contador de positivos
 
 #declaracion de variables
+"""""
+esta funcion lo que hace es que nos da una variable apartir de 0 que cada vez que se escribe un numero se va a sumar al resultado final
+y una vez se escribe un numero 0< termina el contador 
+"""""
 
 def contador_positivos():
     contador =0
@@ -12,10 +16,18 @@ def contador_positivos():
     print("cantidad de numeros positivos ingresados: ", contador)
 
 #definicion de la funcion main
+
+"""""
+llama a la funcion antes escrita
+"""""
 def main ():
     print("bienvenido al contador de positivos")
     contador_positivos()
 
 #llamar a la funcion main para inicia el programa 
+"""
+llama a la funcion main
+"""
+
 if __name__=="__main__":
     main()
