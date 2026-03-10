09859b4059846ae80bdcb2c94d88418b598c408f
Emiliano <rubenemilianomelchorvilla@gmail.com>
2026-03-10 08:42:14 -0600

    calificaiones

---
+++ src/ux.com.curso.programacion/unidad1/tipos_datos/tipo_datos.py
@@ -0,0 +1,17 @@
+#demostracion de tipos de dato en python 
+def datos():
+    entero = 25
+    decimal = 3.14
+    cadena = "hola, mundo"
+    booleano = True
+
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(True)
+
+def main():
+    datos()
+
+if __name__ == "__main__":
+    main()
