7438bb68a82299cf0b1dc598128b2d237d194d4b
emilianO <rubenemilianomelchorvilla@gmail.com>
2026-04-10 08:52:11 -0600

    ejercicio identidades trigonometricas

---
+++ unidad2/jupyter/idenyidad_trigonometrica.py
@@ -0,0 +1,23 @@
+import math
+
+def identidad_trigonometrica(x):
+    numero_radianes = math.radians(x)
+    seno_x = math.pow(math.sin(numero_radianes), 2)
+    coseno_x = math.pow(math.cos(numero_radianes), 2)
+    identidad = seno_x + coseno_x
+    print(f"Para x = {x} grados, sin^2(x) + cos^2(x) = {identidad}")
+
+def identidad_trigonometrica(x):
+    numero_radianes = math.radians(x)
+    seno_x = math.pow(math.sin(numero_radianes), 2)
+    coseno_x = math.pow(math.cos(numero_radianes), 2)
+    identidad = seno_x - coseno_x
+    print(f"Para x = {x} grados, sin^2(x) - cos^2(x) = {identidad}")
+
+def main():
+    x = int(input("Ingrese el valor de x en grados: "))
+    identidad_trigonometrica(x)
+
+if __name__ == "__main__":
+    main()
+    
